99c3db007ee2ed24aeb66ba61981e34722574c3f
Fadhly Al-farizi <fadhlyalfarizi30@gmail.com>
2024-06-24 16:35:12 +0700

    Upload Assignment 01
    - Create Model Contact
    - Create Repository Contact
    - Create Contact Controller
    - Connect Database
    - Testing API Using Postman

---
+++ Week 03/Lecture 05/assignment1/src/main/java/com/example/assignment1/repository/ContactRepository.java
@@ -0,0 +1,9 @@
+package com.example.assignment1.repository;
+
+import com.example.assignment1.model.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, String> {
+}
