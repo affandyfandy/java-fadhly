a84661be8717a630d65ad25b3268064eae5276ed
Fadhly Al-farizi <fadhlyalfarizi30@gmail.com>
2024-08-18 12:36:05 +0700

    Feat: assignemnt 02 - lecture 19

--- Week 10/Lecture 19/Assignment 02/gateway/src/main/java/com/week10/gateway/GatewayApplication.java
+++ Week 10/Lecture 19/Assignment 02/gateway/src/main/java/com/week10/gateway/GatewayApplication.java
@@ -1,13 +1,19 @@
 package com.week10.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator; 
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder; 
-import org.springframework.context.annotation.Bean; 
+import org.springframework.context.annotation.Bean;
+
+import com.week10.gateway.filter.CustomGatewayFilter; 
 
 @SpringBootApplication
 public class GatewayApplication {
+
+    @Autowired
+    private CustomGatewayFilter customGatewayFilter;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
@@ -15,11 +21,14 @@ public class GatewayApplication {
 
 	@Bean
     public RouteLocator routerBuilder(RouteLocatorBuilder routeLocatorBuilder){ 
-        return routeLocatorBuilder.routes() 
-                        .route("post",r->r.path("/api/v2/posts/**") 
-                                .uri("http://localhost:8081/")) 
-                        .route("comment",r->r.path("/api/v2/comments/**") 
-                                .uri("http://localhost:8082/")).build(); 
+        return routeLocatorBuilder.routes()
+                .route("post", r -> r.path("/api/v2/posts/**")
+                        .filters(f -> f.filter(customGatewayFilter.apply(new CustomGatewayFilter.Config())))
+                        .uri("http://localhost:8081/"))
+                .route("comment", r -> r.path("/api/v2/comments/**")
+                        .filters(f -> f.filter(customGatewayFilter.apply(new CustomGatewayFilter.Config())))
+                        .uri("http://localhost:8082/"))
+                .build();
     } 
 
 }
